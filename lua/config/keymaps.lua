5137b23b2fec59e870392af687ac828809b621cd
jfrerich <jasonfrerich@yahoo.com.com>
2024-03-22 23:15:01 -0500

    Remap ',/' to live grep the letters being typed

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
+-- local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
 
 -- " Split Windows
 map("n", "<leader>h", "<cmd>split<CR>")
@@ -9,3 +10,19 @@ map("n", "<leader>v", "<cmd>vsplit<CR>")
 
 -- show buffers
 map("n", ";", "<cmd> Telescope buffers <CR>")
+
+local telescope, builtin = require("telescope"), require("telescope.builtin")
+function fuzzyFindFiles()
+  builtin.grep_string({
+    path_display = { "smart" },
+    only_sort_text = true,
+    word_match = "-w",
+    search = "",
+  })
+end
+
+vim.keymap.set("n", "<leader>/", "<cmd>lua fuzzyFindFiles()<CR>")
+
+-- map("n", "<leader>rg", live_grep_args_shortcuts.grep_word_under_cursor)
+-- local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
+-- map("n", "<leader>gc", live_grep_args_shortcuts.grep_word_under_cursor)
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,34 @@
+local telescope, builtin = require("telescope"), require("telescope.builtin")
+
+telescope.load_extension("fzf")
+
 if vim.fn.executable("rg") == 1 then
-  -- using rg
-  return {}
+  return {
+    "telescope",
+    defaults = {
+      vimgrep_arguments = {
+        "rg",
+        "--line-number",
+        "--no-heading",
+        "--column",
+        "--smart-case",
+        "--silent",
+        "--vimgrep",
+      },
+    },
+    keys = {
+      { "<leader>/", "<cmd>lua fuzzyFindFiles()<CR>" },
+      { "<leader>/", false },
+    },
+  }
 else
   if vim.fn.executable("ag") == 1 then
     return {
       "telescope",
+      keys = {
+        { "<leader>/", false },
+      },
+      { "<leader>/", "<cmd>lua fuzzyFindFiles()<CR>" },
       defaults = {
         vimgrep_arguments = {
           "ag",
