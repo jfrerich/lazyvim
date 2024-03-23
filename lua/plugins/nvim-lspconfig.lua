7de3059b3a7fb496bcb35de18b96e5edf232de71
jfrerich <jasonfrerich@yahoo.com.com>
2024-03-23 02:53:35 -0500

    Add perlPLS

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,24 @@
+require("lspconfig").perlpls.setup({})
+--   "perlnavigator",
+--   settings = {
+--     perlPath = "perl",
+--     enableWarnings = true,
+--     perltidyProfile = "",
+--     perlcriticProfile = "",
+--     perlcriticEnabled = true,
+--   },
+-- }
+
+-- add pyright to lspconfig
+-- {
+--   "neovim/nvim-lspconfig",
+--   ---@class PluginLspOpts
+--   opts = {
+--     ---@type lspconfig.options
+--     servers = {
+--       -- pyright will be automatically installed with mason and loaded with lspconfig
+--       perlnavigator = {},
+--     },
+--   },
+-- }
+-- )
