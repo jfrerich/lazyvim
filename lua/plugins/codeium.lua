92ceb5d891cb2c537b2c35bbfd80e0df0aa03286
jfrerich <jasonfrerich@yahoo.com.com>
2024-10-03 18:11:51 -0500

    add codeium plugin

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,30 @@
+return {
+  "Exafunction/codeium.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    require("codeium").setup({
+      enable_chat = true,
+    })
+  end,
+}
+-- return {
+--   "Exafunction/codeium.vim",
+--   config = function()
+--     -- Change '<C-g>' here to any keycode you like.
+--     vim.keymap.set("i", "<C-g>", function()
+--       return vim.fn["codeium#Accept"]()
+--     end, { expr = true, silent = true })
+--     vim.keymap.set("i", "<c-;>", function()
+--       return vim.fn["codeium#CycleCompletions"](1)
+--     end, { expr = true, silent = true })
+--     vim.keymap.set("i", "<c-,>", function()
+--       return vim.fn["codeium#CycleCompletions"](-1)
+--     end, { expr = true, silent = true })
+--     vim.keymap.set("i", "<c-x>", function()
+--       return vim.fn["codeium#Clear"]()
+--     end, { expr = true, silent = true })
+--   end,
+-- }
